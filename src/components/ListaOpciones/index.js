13af5b4113bafa9851b3c8da736ac07c8671274b
m1994d <marlondiaz2994@gmail.com>
2025-01-02 23:57:34 -0800

    fix

---
+++ src/components/ListaOpciones/index.js
@@ -0,0 +1,27 @@
+import "./ListaOpciones.css";
+const ListaOpciones = () => {
+  //Metodo map -> arreglo.map( (equipo, index) => {
+  //  return <option></option>
+  //})
+  const equipos = [
+    "Programacion",
+    "Front End",
+    "Data Science",
+    "Devops",
+    "UX y Diseño",
+    "Movil",
+    "Innovacion y Gestion",
+  ];
+
+
+  return (
+    <div className="lista-opciones">
+      <label>Equipos</label>
+      <select>
+        {equipos.map((equipo, index) =><option key={index}>{equipo}</option>)}
+      </select>
+    </div>
+  );
+};
+
+export default ListaOpciones;
